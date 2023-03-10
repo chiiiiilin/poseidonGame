1578877248cc719ddc7cf64a8e6aba14b973216e
chiiiii <chei94117@gmail.com>
2023-03-10 23:14:53 +0800

    功能

--- all.js
+++ all.js
@@ -1,6 +1,8 @@
-//舒服滾動
-window.scrollTo({
-    behavior: "smooth",
+//search啦
+const search = document.querySelector(".search");
+let searching = document.querySelector('.searching');
+search.addEventListener("click", () => {
+    searching.classList.toggle("searchinggg");
 });
 
 //僅供pc遊玩按鈕
@@ -22,3 +24,83 @@ if (isMobileDevice()) {
     error.style.display = "none";
 }
 
+//輪播圖
+const navItems = document.querySelectorAll(".slider-nav-item");
+const container = document.querySelector(".slider-wrapper");
+
+const distance = container.offsetWidth / navItems.length;
+let currentIndex = 0;
+let targetIndex = 0;
+let isAnimating = false;
+let timer = null;
+
+const autoSlide = () => {
+    targetIndex = (currentIndex + 1) % navItems.length;
+    switchSlide();
+};
+
+const switchSlide = () => {
+    if (currentIndex !== targetIndex && !isAnimating) {
+        isAnimating = true;
+        navItems[currentIndex].classList.remove("slider-active");
+        navItems[targetIndex].classList.add("slider-active");
+
+        const displacement = -targetIndex * (100 / navItems.length);
+        container.style.transition = "transform 0.5s ease-in-out";
+        container.style.transform = `translateX(${displacement}%)`;
+
+        const currentSlide = document.querySelector(
+            ".slider-wrapper .slider-active"
+        );
+        currentSlide.classList.remove("slider-active");
+        const targetSlide = document.querySelector(
+            `.slider-item:nth-of-type(${targetIndex + 1})`
+        );
+        targetSlide.classList.add("slider-active");
+
+        currentIndex = targetIndex;
+
+        setTimeout(() => {
+            isAnimating = false;
+        }, 500);
+    }
+};
+
+navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+        targetIndex = parseInt(navItem.getAttribute("data-index"));
+        switchSlide();
+        clearInterval(timer);
+        timer = setInterval(autoSlide, 5000);
+    });
+});
+
+timer = setInterval(autoSlide, 5000);
+
+//舒服滾動
+window.scrollTo({
+    behavior: "smooth",
+});
+
+//貓點
+const intro = document.querySelector("#intro");
+const env = document.querySelector("#environment");
+
+window.addEventListener("scroll", () => {
+    const introRect = intro.getBoundingClientRect();
+    const envRect = env.getBoundingClientRect();
+    const introIsVisible =
+        introRect.top <= 500 && introRect.bottom >= window.innerHeight;
+    const envIsVisible = envRect.top <= 1000;
+
+    if (introIsVisible) {
+        document.querySelector("#introDot").style.color = "white";
+        document.querySelector("#envDot").style.color = "#ffffff80";
+    } else if (envIsVisible) {
+        document.querySelector("#envDot").style.color = "white";
+        document.querySelector("#introDot").style.color = "#ffffff80";
+    } else {
+        document.querySelector("#introDot").style.color = "#ffffff80";
+        document.querySelector("#envDot").style.color = "#ffffff80";
+    }
+});
